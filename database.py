969cabd1bbd716e22f7ac423dac4d605eba49400
Fatih <guvenfatih98@gmail.com>
2019-10-17 23:43:56 +0300

    test database

--- database.py
+++ database.py
@@ -40,8 +40,8 @@ class Database():
                 lsatid = cursor.lastrowid
 
                 for short, value in data['rates'].items():
-                    print(short, value)
                     sql = "INSERT INTO totals_records SET total_id=%d, name='%s', value=%f;" & (int(lsatid), short, value)
+                    print(sql)
                     cursor.execute(sql)
 
             except Exception as ex:
--- fixerio.py
+++ fixerio.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# http://data.fixer.io/api/latest?access_key=3dbf1b2bc69f02ee7b1271afb9748f15
-
 import urllib, json, urllib.request
 from database import Database
 
