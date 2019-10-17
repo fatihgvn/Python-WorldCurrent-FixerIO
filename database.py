22ceea64a4ac9a0471b8de2a8333de8d07f28b8a
Fatih <guvenfatih98@gmail.com>
2019-10-17 22:42:09 +0300

    database fixed connection problem

--- database.py
+++ database.py
@@ -19,16 +19,17 @@ class Database():
         records = cursor.fetchall()
 
         tables = ['current_info','totals','totals_records']
+        tblCounter = 0
+        for tbl in records:
+            if tbl[0] in tables:
+                tblCounter += 1
+        if tblCounter != len(tables):
+            self.firstSetup()
 
-        for table in records:
-            print(table)
-            # if table in tables
-        
     def firstSetup(self):
-        cursor = self.db.cursor()
         with open('initialize.sql') as f:
+            cursor = self.db.cursor()
             cursor.execute(f)
-        self.db.commit()
 
     def __del__(self):
         self.db.close()
