0a6f00570600c71753f7cc87c622049b5557097d
Fatih <guvenfatih98@gmail.com>
2019-10-17 20:12:07 +0300

    initialize

---
+++ server.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# http://data.fixer.io/api/latest?access_key=3dbf1b2bc69f02ee7b1271afb9748f15
+
+import urllib, json, MySQLdb
+
+settings = None
+
+with open('settings.json') as json_file:
+    settings = json.load(json_file)
+
+print(settings)
