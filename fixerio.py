053b31229063112a220a3cde1fa478acccf578df
Fatih <guvenfatih98@gmail.com>
2019-10-20 20:11:59 +0300

    add argparse

--- fixerio.py
+++ fixerio.py
@@ -3,17 +3,30 @@
 
 import urllib, json, urllib.request
 from database import Database
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-g","--getNew", help="add new current record", action="store_true")
+parser.add_argument("-u","--updateHistory", help="Update last day history", action="store_true")
+args = parser.parse_args()
+
+print(len(args.values))
 
 settings = None
 
 with open('settings.json') as json_file:
     settings = json.load(json_file)
 
-apiurl = "http://data.fixer.io/api/latest?access_key=%s" % (settings['token'])
+if args.getNew:
 
-db = Database(settings)
+    apiurl = "http://data.fixer.io/api/latest?access_key=%s" % (settings['token'])
 
-with urllib.request.urlopen(apiurl) as url:
-    data = json.loads(url.read().decode())
+    db = Database(settings)
 
-    db.addRecord(data)
+    with urllib.request.urlopen(apiurl) as url:
+        data = json.loads(url.read().decode())
+
+        db.addRecord(data)
+
+elif args.updateHistory:
+    print("update history")
