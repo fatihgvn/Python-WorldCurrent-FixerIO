8a406b3e35ff72c66fdc6cc3f5b0e9f0465701e2
Fatih <guvenfatih98@gmail.com>
2019-10-20 20:20:55 +0300

    fixed args count

--- fixerio.py
+++ fixerio.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import urllib, json, urllib.request
+import urllib, json, urllib.request, sys
 from database import Database
 import argparse
 
@@ -10,14 +10,12 @@ parser.add_argument("-g","--getNew", help="add new current record", action="stor
 parser.add_argument("-u","--updateHistory", help="Update last day history", action="store_true")
 args = parser.parse_args()
 
-print(len(args.values))
-
 settings = None
 
 with open('settings.json') as json_file:
     settings = json.load(json_file)
 
-if args.getNew:
+if args.getNew or len(sys.argv) == 1:
 
     apiurl = "http://data.fixer.io/api/latest?access_key=%s" % (settings['token'])
 
